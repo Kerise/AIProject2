c5da1e60485a41d1c57abc4c2f3ba89ac51a8c1f
michallachowsky <michal.lachowsky@gmail.com>
2020-12-17 21:58:18 +0100

    LoginCSS

--- views/login.php
+++ views/login.php
@@ -1,10 +1,29 @@
 <?php
 /** @var $model \app\models\User */
 ?>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="./loginStyle.css">
+</head>
+<body>
+<div id="login-wrapper">
+    <img src="./avatar.png" alt="">
 
-<h1>Login</h1>
-<?php $form = \app\Core\form\Form::begin('', "post") ?>
-<?php echo $form->field($model, 'email') ?>
-<?php echo $form->field($model, 'password')->passwordField() ?>
-<button type="submit" class="btn btn-primary">Submit</button>
-<?php \app\Core\form\Form::end() ?>
+    <div id="logowanie">
+        <h1>Login</h1>
+    </div>
+    <div id="inputElements">
+        <?php $form = \app\Core\form\Form::begin('', "post") ?>
+        <?php echo $form->field($model, 'email') ?>
+    </div>
+
+    <div id="inputElements">
+        <?php echo $form->field($model, 'password')->passwordField() ?>
+    </div>
+    <button type="submit" class="btn btn-primary">Submit</button>
+    <?php \app\Core\form\Form::end() ?>
+</div>
+</body>
