6b5365c626236fcd4f40c84cde95ab3cdeb7b318
michallachowsky <michal.lachowsky@gmail.com>
2021-01-18 19:43:00 +0100

    BOG CSS
    Tak to ja bog css

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -15,39 +15,53 @@ use \app\Core\Application;
 </head>
 
 <body>
+
 <div id="menu">
 <?php if(Application::isGuest()){header("Location:http://localhost:8080/login");}
 $role=Application::getRole();
 if($role!=2):?>
+    <div id="home2">
     <a href="/home">
         <div class="option" id="home">Dodaj Fakture</div>
     </a>
+    </div>
 
+    <div id="licence2">
     <a href="/addlicence">
         <div class="option" id="licence">Dodaj Licencje</div>
     </a>
+    </div>
+    <div id="device2">
     <a href="/adddevice">
         <div class="option" id="device">Dodaj Sprzęt</div>
     </a>
+    </div>
     <?php endif; ?>
+    <div id="licencje">
     <a href="/licences">
         <div class="option">Licencje </div>
     </a>
+    </div>
+    <div id="faktury">
     <a href="/">
         <div class="option">Faktury </div>
     </a>
+    </div>
+    <div id="sprzet">
     <a href="/devices">
         <div class="option">Sprzęt </div>
     </a>
+    </div>
     <?php if(Application::isGuest()):?>
     <?php else: ?>
     <?php $role = Application::getRole() ;
     if($role==1):
     ?>
+    <div id="rejestracja">
         <a href="/register">
             <div class="option">Rejestracja </div>
         </a>
-
+    </div>
 
     <?php endif; ?>
         <a href="/logout">
@@ -63,6 +77,7 @@ if($role!=2):?>
     <?php endif; ?>
 
 </div>
+
 
 <div class="container">
         <?php if (Application::$app->session->getFlash('success')): ?>
